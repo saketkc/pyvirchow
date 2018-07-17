9f415e970d4e8f60469b3f7973baea22eed74d77
Saket Choudhary <saketkc@gmail.com>
2018-07-17 13:32:18 +0530

    Update

--- pywsi/segmentation/__init__.py
+++ pywsi/segmentation/__init__.py
@@ -3,5 +3,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .poisson_fit import poisson_deconvolve
 from .graph_cut import perform_binary_cut
+from .helpers import collapse_labels
+from .helpers import collapse_small_area
+from .max_clustering import max_clustering
+from .poisson_fit import poisson_deconvolve
+
+
--- pywsi/segmentation/graph_cut.py
+++ pywsi/segmentation/graph_cut.py
@@ -4,8 +4,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import numpy as np
-import maxflow
-
 import gco
 
 
